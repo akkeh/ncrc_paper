619a2da9af6ad2d8962d0828f535e9b4a4c37ddf
akke <akkemats.houben@ub.edu>
2026-02-05 13:08:25 +0100

    update license

--- __init__.py
+++ __init__.py
@@ -1,2 +1,22 @@
+'''
+    ncrc: neuronal culture reservoir computing
+        This work is accompanied by a scientific publication.
+        Please cite: A.M. Houben, A.-C. Haeb, J. Garcia-Ojalvo \& J. Soriano, Reservoir computing in simulated neuronal cultures: Effect of network structure, (in press)
+    Copyright (C) 2025 Akke Mats Houben (akke@akkehouben.net)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 from . import nc_sim
 from . import rc
